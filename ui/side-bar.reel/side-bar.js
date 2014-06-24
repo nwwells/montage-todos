0bd9c215ec13c1fc0724851a3be12c2e1be19094
Nathan Wells <nwwells@gmail.com>
2014-06-24 20:33:59 +0000

    Add component side-bar

---
+++ ui/side-bar.reel/side-bar.js
@@ -0,0 +1,17 @@
+/**
+ * @module ui/side-bar.reel
+ * @requires montage/ui/component
+ */
+var Component = require("montage/ui/component").Component;
+
+/**
+ * @class SideBar
+ * @extends Component
+ */
+exports.SideBar = Component.specialize(/** @lends SideBar# */ {
+    constructor: {
+        value: function SideBar() {
+            this.super();
+        }
+    }
+});
